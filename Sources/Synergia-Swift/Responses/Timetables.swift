db6718404578e2355380ac1c55d059c798daa112
aeoliux <crewmateminecraft@gmail.com>
2024-07-27 17:04:44 +0200

    Changed method to store information about hours in timetable

--- Sources/Synergia-Swift/Responses/Timetables.swift
+++ Sources/Synergia-Swift/Responses/Timetables.swift
@@ -25,27 +25,24 @@ struct SynergiaTimetable: Decodable {
 
 /// Entry in timetable
 public final class TimetableItem {
-    /// Lesson number
-    public var lessonNo: UInt8
     /// Subject name
     public var subject: String
     /// Teacher's name
     public var teacher: String
     /// If the event was canceled
     public var isCanceled: Bool
-    /// Date of the start
-    public var dateStarts: Date
-    /// Date of the end
-    public var dateEnds: Date
+    /// Starts at
+    public var startsAt: String
+    /// Ends at
+    public var endsAt: String
     /// Classroom
     public var classroom: String = ""
     
-    init(_ timetable: SynergiaTimetable.SynergiaTimetableItem, day: String) {
-        self.lessonNo = UInt8(timetable.LessonNo) ?? 0
+    init(_ timetable: SynergiaTimetable.SynergiaTimetableItem) {
         self.subject = timetable.Subject.Name
         self.teacher = timetable.Teacher.FirstName + " " + timetable.Teacher.LastName
         self.isCanceled = timetable.IsCanceled
-        self.dateStarts = (day + " " + timetable.HourFrom).toDate(withoutSeconds: true) ?? Date()
-        self.dateEnds = (day + " " + timetable.HourTo).toDate(withoutSeconds: true) ?? Date()
+        self.startsAt = timetable.HourFrom
+        self.endsAt = timetable.HourTo
     }
 }
